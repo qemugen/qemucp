76a00b99d171f7181feabdb76745b0cd7bd4a422
QemuCP Dev <soporte@qemugen.com>
2026-04-11 22:43:08 +0000

    fix: hst-install.sh downloads ubuntu installer from fork
    Was downloading hst-install-ubuntu.sh from official HestiaCP repo
    which showed the original Hestia ASCII banner.
    Now downloads from qemugen/qemucp fork which has QemuCP branding.

--- install/hst-install.sh
+++ install/hst-install.sh
@@ -105,7 +105,7 @@ check_wget_curl() {
 		# 		exit 1
 		# 	fi
 		# else
-		wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh -O hst-install-$type.sh
+		wget -q https://raw.githubusercontent.com/qemugen/qemucp/release/install/hst-install-$type.sh -O hst-install-$type.sh
 		if [ "$?" -eq '0' ]; then
 			bash hst-install-$type.sh $*
 			exit
@@ -128,7 +128,7 @@ check_wget_curl() {
 		# 		exit 1
 		# 	fi
 		# else
-		curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh
+		curl -s -O https://raw.githubusercontent.com/qemugen/qemucp/release/install/hst-install-$type.sh
 		if [ "$?" -eq '0' ]; then
 			bash hst-install-$type.sh $*
 			exit
