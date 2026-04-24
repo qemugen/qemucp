362a7de2c975b350696a41473f2b475cc986d251
QemuCP <soporte@qemugen.com>
2026-04-24 12:04:50 +0000

    feat: Add WP icon next to WP label in panel menu tab

--- web/templates/includes/panel.php
+++ web/templates/includes/panel.php
@@ -360,7 +360,7 @@
                     <a class="main-menu-item-link <?php if ($TAB == "WP") {
     echo "active"; } ?>" href="/list/wp/" title="<?= _("WordPress Manager") ?>">
                         <i class="fab fa-wordpress"></i>
-                        <p class="main-menu-item-label"><?= _("WP") ?><i class="fab fa-wordpress"></i></p>
+                        <p class="main-menu-item-label"><?= _("WP") ?> <i class="fab fa-wordpress" style="font-size:11px;vertical-align:middle;"></i></p>
                         <p class="main-menu-stats"></p>
                     </a>
                 </li>
